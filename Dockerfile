97d33fd1a8543a66acea82419917b88f69b020e7
bashiru98 <atulebashirubukari@gmail.com>
2021-04-05 02:54:52 +0300

    added

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,6 @@ RUN  npm install
 
 COPY . .
 
-EXPOSE 5000
+EXPOSE 6000
 
 CMD ["npm", "start"]
--- server.js
+++ server.js
@@ -72,7 +72,7 @@ const PORT = process.env.PORT || 5000;
 const server = app.listen(
   PORT,
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+    `Server running in ${process.env.NODE_ENV} mode on port this ${PORT}`.yellow.bold
   )
 );
 
